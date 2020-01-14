0f7a34127b6daf0fa996fa36308568be5c08517d
huaqingyi <yihuaqing@talefun.com>
2020-01-14 16:36:44 +0800

    init

--- src/application.ts
+++ src/application.ts
@@ -1,7 +1,57 @@
-import { PYIBootstrap, PYIApplication, PYIOnInitApplication } from 'pyi';
-import { SwaggerInjectService, Swagger } from 'pyi-swagger';
-SwaggerInjectService.register();
+import {
+    PYIBootstrap, PYIApplication, PYIOnInitApplication,
+    PYIOnInit, PYIOnScanInit, PYIOnScanChange, PYIOnScanAfter,
+    PYIOnConfigurationInit, PYIOnConfigurationAfter,
+    PYIOnPluginApplication,
+    PYIAppConfiguration
+} from 'pyi';
+import { SwaggerInjectService } from 'pyi-swagger';
 
 @PYIBootstrap
-export class Application extends PYIApplication<any, any> {
+export class Application extends PYIApplication<any, any> implements
+    PYIOnInit,
+    PYIOnScanInit,
+    PYIOnScanChange,
+    PYIOnScanAfter,
+    PYIOnConfigurationInit,
+    PYIOnConfigurationAfter,
+    PYIOnInitApplication,
+    PYIOnPluginApplication {
+
+    constructor() {
+        super();
+    }
+
+    public async onInit() {
+        await SwaggerInjectService.register();
+        console.log('onInit ...');
+    }
+
+    public async onScanInit() {
+        console.log('onScanInit ...');
+    }
+
+    public async onScanChange() {
+        console.log('onScanChange ...');
+    }
+
+    public async onScanAfter() {
+        console.log('onScanAfter ...');
+    }
+
+    public async onConfigurationInit() {
+        console.log('onConfigurationInit ...');
+    }
+
+    public async onConfigurationAfter(config: PYIAppConfiguration) {
+        console.log('onConfigurationAfter ...');
+    }
+
+    public async onInitApplication() {
+        console.log('onInitApplication ...');
+    }
+
+    public async onPluginApplication() {
+        console.log('onPluginApplication ...');
+    }
 }
